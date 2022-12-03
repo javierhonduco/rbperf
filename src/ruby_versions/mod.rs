4d5cc38e6011f915cde5f7c4b1dab56e7245c458
Francisco Javier Honduvilla Coto <javierhonduco@gmail.com>
2022-12-03 14:03:36 +0000

    Add support for Ruby 3.1.3
    Signed-off-by: Francisco Javier Honduvilla Coto <javierhonduco@gmail.com>

--- src/ruby_versions/mod.rs
+++ src/ruby_versions/mod.rs
@@ -6,3 +6,4 @@ pub const ruby_2_7_6: &str = include_str!("ruby_2_7_6.yaml");
 pub const ruby_3_0_0: &str = include_str!("ruby_3_0_0.yaml");
 pub const ruby_3_0_4: &str = include_str!("ruby_3_0_4.yaml");
 pub const ruby_3_1_2: &str = include_str!("ruby_3_1_2.yaml");
+pub const ruby_3_1_3: &str = include_str!("ruby_3_1_3.yaml");
