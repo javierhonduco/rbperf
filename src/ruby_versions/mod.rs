aae311d8a09111d86461b73f8ea96736cbf19123
Francisco Javier Honduvilla Coto <javierhonduco@gmail.com>
2023-02-19 19:18:14 +0000

    Make Ruby config indices explicit and easily queriable
    Signed-off-by: Francisco Javier Honduvilla Coto <javierhonduco@gmail.com>

--- src/ruby_versions/mod.rs
+++ src/ruby_versions/mod.rs
@@ -1,9 +1,11 @@
-pub const ruby_2_6_0: &str = include_str!("ruby_2_6_0.yaml");
-pub const ruby_2_6_3: &str = include_str!("ruby_2_6_3.yaml");
-pub const ruby_2_7_1: &str = include_str!("ruby_2_7_1.yaml");
-pub const ruby_2_7_4: &str = include_str!("ruby_2_7_4.yaml");
-pub const ruby_2_7_6: &str = include_str!("ruby_2_7_6.yaml");
-pub const ruby_3_0_0: &str = include_str!("ruby_3_0_0.yaml");
-pub const ruby_3_0_4: &str = include_str!("ruby_3_0_4.yaml");
-pub const ruby_3_1_2: &str = include_str!("ruby_3_1_2.yaml");
-pub const ruby_3_1_3: &str = include_str!("ruby_3_1_3.yaml");
+pub const ruby_version_configs_yaml: &[&str] = &[
+    include_str!("ruby_2_6_0.yaml"),
+    include_str!("ruby_2_6_3.yaml"),
+    include_str!("ruby_2_7_1.yaml"),
+    include_str!("ruby_2_7_4.yaml"),
+    include_str!("ruby_2_7_6.yaml"),
+    include_str!("ruby_3_0_0.yaml"),
+    include_str!("ruby_3_0_4.yaml"),
+    include_str!("ruby_3_1_2.yaml"),
+    include_str!("ruby_3_1_3.yaml"),
+];
