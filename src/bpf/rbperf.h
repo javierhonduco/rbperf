049093ee5747f5d91abd985da32739393399dff8
Francisco Javier Honduvilla Coto <javierhonduco@gmail.com>
2023-08-20 13:41:41 +0100

    Add missing copyright header
    Signed-off-by: Francisco Javier Honduvilla Coto <javierhonduco@gmail.com>

--- src/bpf/rbperf.h
+++ src/bpf/rbperf.h
@@ -2,6 +2,8 @@
 //
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the root directory of this source tree.
+//
+// Copyright (c) 2022 The rbperf authors
 
 #include "basic_types.h"
 
